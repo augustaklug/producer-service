abab44afbd800a7b826e9681eada669338795fea
Augusta Carla Klug <augustaklug@gmail.com>
2024-07-31 12:39:00 -0300

    actuator

--- src/main/java/com/klug/producer/controller/ProducerController.java
+++ src/main/java/com/klug/producer/controller/ProducerController.java
@@ -1,2 +1,13 @@
-package com.klug.producer.controller;public class ProducerController {
+package com.klug.producer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+    @GetMapping("/produce")
+    public String produce() {
+        return "Hello from Producer!";
+    }
 }
